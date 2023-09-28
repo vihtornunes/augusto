4133653839916ec776d21eb1e3d7cb0442203b89
Vitor Nunes <112831617+vihtornunes@users.noreply.github.com>
2023-09-28 15:25:01 -0300

    commit 28/09/23

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 AOS.init();
+
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion h2');
+accordionList[0].classList.add('ativo');
+accordionList[0].nextElementSibling.classList.add('ativo');
+
+function activeAccordion() {
+    this.classList.toggle('ativo');
+    this.nextElementSibling.classList.toggle('ativo');
+}
+
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+});
+}
+initAccordion();
